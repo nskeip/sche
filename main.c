8e5688d099e9b7244ecd9647debf2ee5e2b21734
Nikita Hismatov <hismatovns@gmail.com>
2023-11-11 22:47:11 +0400

    tokenizer (red)

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <assert.h>
+#include <ctype.h>
 #include <stdbool.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -17,19 +19,67 @@ typedef struct {
 
 typedef struct {
   bool ok;
-  Token tokens[MAX_TOKENS];
+  union {
+    Token tokens[MAX_TOKENS];
+    struct {
+      size_t line_no;
+      size_t char_no;
+    };
+  };
 } TokenizerResult;
 
 TokenizerResult tokenize(const char *s) {
-  TokenizerResult result;
-  for (size_t i = 0; *s != '\0' && i < MAX_TOKENS; ++s, ++i) {
-    printf("%zu) %c\n", i, *s);
+  TokenizerResult result = {.ok = true, .tokens = {0}};
+  size_t line_i = 0;
+  for (size_t token_count = 0, char_i = 0;
+       *s != '\0' && token_count < MAX_TOKENS; ++s, ++char_i) {
+    char c = *s;
+    if (isspace(c)) {
+      if (c == '\n') {
+        ++line_i;
+      }
+    } else if (isdigit(c)) {
+      result.tokens[token_count].type = Number;
+      result.tokens[token_count].value.i = atoi(s);
+      while (isdigit(*(s + 1))) {
+        ++s;
+      }
+    } else if (c == '(' || c == ')') {
+      result.tokens[token_count].type = Paren;
+      result.tokens[token_count].value.c = c;
+    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+      result.tokens[token_count].type = Op;
+      result.tokens[token_count].value.c = c;
+    } else {
+      TokenizerResult err_result = {
+          .ok = false, .line_no = line_i, .char_no = char_i};
+      return err_result;
+    }
+
+    ++token_count;
   }
   return result;
 }
 
 int main(void) {
-  TokenizerResult tr = tokenize("(+ 1 2)");
-  assert(tr.ok);
+  {
+    TokenizerResult tr = tokenize("(+ 1 20)");
+    assert(tr.ok);
+
+    assert(tr.tokens[0].type == Paren);
+    assert(tr.tokens[0].value.c == '(');
+
+    assert(tr.tokens[1].type == Op);
+    assert(tr.tokens[1].value.c == '+');
+
+    assert(tr.tokens[2].type == Number);
+    assert(tr.tokens[2].value.i == 1);
+
+    assert(tr.tokens[3].type == Number);
+    assert(tr.tokens[3].value.i == 20);
+
+    assert(tr.tokens[4].type == Paren);
+    assert(tr.tokens[4].value.c == ')');
+  }
   return EXIT_SUCCESS;
 }
